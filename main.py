e200603284a1771e7c51ca8bf9a7fbbb0e50f9f0
LodnIFMT <nascimento.lucas@estudante.ifmt.edu.br>
2025-01-24 13:43:57 -0400

    pesquisa por texto feita

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
+import openpyxl
+from time import sleep
 
+planilha = "Lista_Jogos.xlsx"
 
+def pesquisar(Arquivo):
+    wb = openpyxl.load_workbook(Arquivo)
+    sheet = wb.active
 
+    nome_jogo = input("Nome do Jogo: ").lower()
+
+    ctd = 0
+    for linha in sheet.iter_rows(min_row=2, min_col=2, max_col=7):
+        ctd = ctd+1
+
+        if linha[0].value.lower() == nome_jogo.lower():
+            print("\n          !!! JOGO ENCONTRADO !!!")
+            print(f'\nNome ------------: {linha[0].value}')
+            print(f'Desenvolvedor ---: {linha[5].value}')
+            print(f'Plataforma: -----: {linha[1].value}')
+            print(f'Lançamento ------: {linha[2].value}')
+            break
+
+        if ctd == sheet.max_row -1:
+            print(f"         !!!O Jogo {nome_jogo.title()} Não Foi Encontrado !!!")
+
+pesquisar(planilha)
