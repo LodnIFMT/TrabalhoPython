75500ddfcc9fb4c5d8a0df0fd16864c9822bf35c
LodnIFMT <nascimento.lucas@estudante.ifmt.edu.br>
2025-01-24 14:33:07 -0400

    Update main.py
    Eu Só comentei, pra ter uma noção doque está acontecendo

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-import openpyxl
-from time import sleep
+import openpyxl # biblioteca para manipular arquivos xlsx (Excel)
+#from time import sleep  //Biblioteca usada para testes
 
-planilha = "Lista_Jogos.xlsx"
+planilha = "Lista_Jogos.xlsx" # Variavel do caminho da planilha
 
 def pesquisar(Arquivo):
+    #Abre o Arquivo
     wb = openpyxl.load_workbook(Arquivo)
     sheet = wb.active
 
     nome_jogo = input("Nome do Jogo: ").lower()
 
-    ctd = 0
-    for linha in sheet.iter_rows(min_row=2, min_col=2, max_col=7):
+    ctd = 0 # contador
+    for linha in sheet.iter_rows(min_row=2, min_col=2, max_col=7): #Para cada Linha Da Planilha...
         ctd = ctd+1
 
-        if linha[0].value.lower() == nome_jogo.lower():
+        if linha[0].value.lower() == nome_jogo.lower(): #Se o Valor da linha for igual o nome do jogo do usuario...
             print("\n          !!! JOGO ENCONTRADO !!!")
             print(f'\nNome ------------: {linha[0].value}')
             print(f'Desenvolvedor ---: {linha[5].value}')
@@ -21,7 +22,7 @@ def pesquisar(Arquivo):
             print(f'Lançamento ------: {linha[2].value}')
             break
 
-        if ctd == sheet.max_row -1:
+        if ctd == sheet.max_row -1: # Se contador for igual a ultima linha (com conteudo) da planilha...
             print(f"         !!!O Jogo {nome_jogo.title()} Não Foi Encontrado !!!")
 
 pesquisar(planilha)
