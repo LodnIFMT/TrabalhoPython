19aaf88d19f3a7c9257eda220611d1dd99c698e4
LodnIFMT <nascimento.lucas@estudante.ifmt.edu.br>
2025-01-31 17:48:46 -0400

    Saga Da Interface

--- interface.py
+++ interface.py
@@ -1,4 +1,6 @@
 import customtkinter as ctk
+import json
+import os
 
 class App(ctk.CTk):
     def __init__(self):
@@ -7,38 +9,59 @@ class App(ctk.CTk):
         self.title("Jogo") #Titulo da Janela
         self.geometry("1080x720") #Tamanho da Janela
 
+        # Verifica se o arquivo data.json existe
+        if not os.path.exists("data.json"):
+            while True:
+                self.caminho = ctk.CTkInputDialog(text='Diretório da Planilha:') #Cria a janela de dialogo para o usuario digitar o caminho da planilha
+                self.caminho_input = self.caminho.get_input().replace('"', '') #Retira as aspas do caminho
+                if self.caminho_input != '' and os.path.exists(self.caminho_input): #Verifica se o caminho é valido
+                    self.info = {"Planilha": self.caminho_input} #Cria um dicionario com o caminho da planilha
+
+                    with open("data.json", "w") as f: #Cria o arquivo data.json
+                        json.dump(self.info, f) #Escreve o caminho da planilha no arquivo
+
+                    break #Sai do loop  
+
         #Area de Pesquisar o Jogo pelo Nome
         self.label = ctk.CTkLabel(self, text="Nome do Jogo",) #Label escrito, Nome do jogo
-        self.nome_jogo = ctk.CTkEntry(self, placeholder_text="ex: Dragon Ball Z", width=500) #Campo para pesquisar o jogo
-        self.label.grid(row=0, column=0) #Insere a Label na janela
-        self.nome_jogo.grid(row=1, column=0, padx=5, pady=5) #Insere O Campo Para Digitar o nome do Jogo, na Janela
+        self.nome_jogo = ctk.CTkEntry(self, placeholder_text="ex: Dragon Ball Z", width=550) #Campo para pesquisar o jogo
+        self.label.grid(row=0, column=0, sticky="w", padx=12) #Insere a Label na janela
+        self.nome_jogo.grid(row=1, column=0, padx=10, pady=5, sticky="w") #Insere O Campo Para Digitar o nome do Jogo, na Janela
+        self.grid_columnconfigure(0, weight=2)
 
         #Area de Filtrar pelo Desenvolvedor
         self.label_Dev = ctk.CTkLabel(self, text="Desenvolvedor") #Label escrito, Desenvolvedor
-        self.opcao_devs = ctk.CTkOptionMenu(self, values=["Desenvolvedor1", "Desenvolvedor2", "Desenvolvedor3"]) #Menu de opções, que lista todos os Desenvolvedores
+        self.opcao_devs = ctk.CTkOptionMenu(self, values=["Desenvolvedor1", "Desenvolvedor2", "Desenvolvedor3"], width=250) #Menu de opções, que lista todos os Desenvolvedores
         self.label_Dev.grid(row=0, column=1) #Insere a Label na janela
-        self.opcao_devs.set("Desenvolvedores") #Fica Escrito, Desenvolvedores, quando o usuario não seleciona a opção
+        self.opcao_devs.set("-----") #quando o usuario não seleciona a opção
         self.opcao_devs.grid(row=1, column=1, padx=5, pady=5) #Adiciona o Menu na Janela
+        self.grid_columnconfigure(1, weight=1)
 
         #Area de filtrar por plataforma
         self.label_plataforma = ctk.CTkLabel(self, text="Plataforma") #Label escrito, Plataforma
-        self.opcao_plataforma = ctk.CTkOptionMenu(self, values=["PC", "X360", "PS", "PS2"]) #Menu de opçôes que lista todas as plataforma
+        self.opcao_plataforma = ctk.CTkOptionMenu(self, values=["PC", "X360", "PS", "PS2"], width=250) #Menu de opçôes que lista todas as plataforma
         self.label_plataforma.grid(row=0, column=2) #Insere a Label na janela
-        self.opcao_plataforma.set("Plataforma") #Fica Escrito, Plataforma, quando o usuario não seleciona a opção
+        self.opcao_plataforma.set("-----") #quando o usuario não seleciona a opção
         self.opcao_plataforma.grid(row=1, column=2, padx=5, pady=5)#Adiciona o Menu a janeeal
+        self.grid_columnconfigure(2, weight=1)
 
         #Area de filtrar por Ano
         self.label_ano = ctk.CTkLabel(self, text="Ano") #Label escrito, Ano
-        self.opcao_ano = ctk.CTkOptionMenu(self, values=["2000", "2001", "2002", "2003", "2004"]) #Menu de opções que lista todos os anos que tem na planilha
+        self.opcao_ano = ctk.CTkOptionMenu(self, values=["2000", "2001", "2002", "2003", "2004"], width=250,) #Menu de opções que lista todos os anos que tem na planilha
         self.label_ano.grid(row=0, column=3) #Insere a Label na janela
-        self.opcao_ano.set("Ano") #Fica Escrito, Ano, quando o usuario não seleciona a opção
+        self.opcao_ano.set("-----") #Fica Escrito, Ano, quando o usuario não seleciona a opção
         self.opcao_ano.grid(row=1, column=3, padx=5, pady=5) #Adiciona o Menu a janeeal
+        self.grid_columnconfigure(3, weight=1)
 
         #Botão Buscar
-        self.botao = ctk.CTkButton(self, text="BUSCAR", width=110) #Cria o botao
+        self.botao = ctk.CTkButton(self, text="BUSCAR", width=150) #Cria o botao
         self.botao.grid(row=1, column=4, padx=5, pady=5) #Adiciona o botão
+        self.grid_columnconfigure(4, weight=1)
 
-        
+        # Campo para exibir as informações dos jogos
+        self.campo_texto = ctk.CTkTextbox(self, width=580, height=635)
+        self.campo_texto.grid(row=2, column=0, columnspan=3, padx=10, pady=5, sticky="w")
+        self.grid_rowconfigure(2, weight=1)
 
 app = App()
 app.mainloop()
--- main.py
+++ main.py
@@ -1,28 +1,43 @@
-import openpyxl # biblioteca para manipular arquivos xlsx (Excel)
-#from time import sleep  //Biblioteca usada para testes
+import openpyxl
+import os
+import json
 
-planilha = "InfoJogo.xlsx" # Variavel do caminho da planilha
+# with open("data.json", "r") as f:
+#     data = json.load(f)
+#     planilha = data["Planilha"]
 
-def pesquisar(Arquivo):
-    #Abre o Arquivo
-    wb = openpyxl.load_workbook(Arquivo)
-    sheet = wb.active
+# def pesquisar(Arquivo):
+#     #Abre o Arquivo
+#     wb = openpyxl.load_workbook(Arquivo)
+#     sheet = wb.active
 
-    nome_jogo = input("Nome do Jogo: ").lower()
+#     nome_jogo = input("Nome do Jogo: ").lower()
 
-    ctd = 0 # contador
-    for linha in sheet.iter_rows(min_row=2, min_col=2, max_col=7): #Para cada Linha Da Planilha...
-        ctd = ctd+1
+#     ctd = 0 # contador
+#     for linha in sheet.iter_rows(min_row=2, min_col=2, max_col=7): #Para cada Linha Da Planilha...
+#         ctd = ctd+1
 
-        if linha[0].value.lower() == nome_jogo.lower(): #Se o Valor da linha for igual o nome do jogo do usuario...
-            print("\n          !!! JOGO ENCONTRADO !!!")
-            print(f'\nNome ------------: {linha[0].value}')
-            print(f'Desenvolvedor ---: {linha[5].value}')
-            print(f'Plataforma: -----: {linha[1].value}')
-            print(f'Lançamento ------: {linha[2].value}')
-            break
+#         if linha[0].value.lower() == nome_jogo.lower(): #Se o Valor da linha for igual o nome do jogo do usuario...
+#             print("\n          !!! JOGO ENCONTRADO !!!")
+#             print(f'\nNome ------------: {linha[0].value}')
+#             print(f'Desenvolvedor ---: {linha[5].value}')
+#             print(f'Plataforma: -----: {linha[1].value}')
+#             print(f'Lançamento ------: {linha[2].value}')
+#             break
 
-        if ctd == sheet.max_row -1: # Se contador for igual a ultima linha (com conteudo) da planilha...
-            print(f"         !!!O Jogo {nome_jogo.title()} Não Foi Encontrado !!!")
+#         if ctd == sheet.max_row -1: # Se contador for igual a ultima linha (com conteudo) da planilha...
+#             print(f"         !!!O Jogo {nome_jogo.title()} Não Foi Encontrado !!!")
 
-pesquisar(planilha)
+# pesquisar(planilha)
+
+class Arquivo():
+    def __init__(self):
+        super().__init__()
+
+    def arquivo(self):
+        with open("data.json", "r") as f:
+            data = json.load(f)
+            planilha = data["Planilha"]
+        return planilha
+    
+    
