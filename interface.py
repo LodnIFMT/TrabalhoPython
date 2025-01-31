9ad01fd093cef8291b95fd1f43cf162db845410d
LodnIFMT <nascimento.lucas@estudante.ifmt.edu.br>
2025-01-31 09:30:48 -0400

    Fazendo Interface

--- interface.py
+++ interface.py
@@ -1,14 +1,32 @@
-import tkinter as tk 
+import customtkinter as ctk
 
-janela = tk.Tk()
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
 
-janela.title("Pesquisar Jogos")
-janela.geometry("650x650")
+        self.title("Jogo") #Titulo da Janela
+        self.geometry("1080x720") #Tamanho da Janela
 
-tk.Label(janela, text="Nome do jogo:").grid(row=0, column=0, pady=2)
-nome = tk.Entry(janela, width=50)
-nome.grid(row=1,column=0, padx=5, pady=5)
+        #Area de Pesquisar o Jogo pelo Nome
+        self.label = ctk.CTkLabel(self, text="Nome do Jogo",) #Label escrito, Nome do jogo
+        self.nome_jogo = ctk.CTkEntry(self, placeholder_text="ex: Dragon Ball Z", width=500) #Campo para pesquisar o jogo
+        self.label.grid(row=0, column=0) #Insere a Label na janela
+        self.nome_jogo.grid(row=1, column=0, padx=5, pady=5) #Insere O Campo Para Digitar o nome do Jogo, na Janela
 
-dev-menu = tk.OptionMenu(janela, )
+        #Area de Filtrar pelo Desenvolvedor
+        self.opcao_devs = ctk.CTkOptionMenu(self, values=["Desenvolvedor1", "Desenvolvedor2", "Desenvolvedor3"]) #Menu de opções, que lista todos os Desenvolvedores
+        self.opcao_devs.set("Desenvolvedores") #Fica Escrito, Desenvolvedores, quando o usuario não seleciona a opção
+        self.opcao_devs.grid(row=1, column=3, padx=5, pady=5) #Adiciona o Menu na Janela
 
-janela.mainloop()
+        #Area de filtrar por plataforma
+        self.opcao_plataforma = ctk.CTkOptionMenu(self, values=["PC", "X360", "PS", "PS2"]) #Menu de opçôes que lista todas as plataforma
+        self.opcao_plataforma.set("Plataforma") #Fica Escrito, Plataforma, quando o usuario não seleciona a opção
+        self.opcao_plataforma.grid(row=1, column=4, padx=5, pady=5)#Adiciona o Menu a janeeal
+
+        #Area de filtrar por Ano
+        self.opcao_ano = ctk.CTkOptionMenu(self, values=["2000", "2001", "2002", "2003", "2004"]) #Menu de opções que lista todos os anos que tem na planilha
+        self.opcao_ano.set("Ano") #Fica Escrito, Ano, quando o usuario não seleciona a opção
+        self.opcao_ano.grid(row=1, column=5, padx=5, pady=5) #Adiciona o Menu a janeeal
+
+app = App()
+app.mainloop()
